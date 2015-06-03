30eec9c0da96c3899603fc587a0df7bb8452b590
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-06-03 18:32:13 +0000

    initial commit

---
+++ Projects/fizzbuzz.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        input=int(raw_input('Please enter a three-digit number: '))
+        break
+    except ValueError:
+        print "Please enter a number!"
+
+def fizzbuzz(input):
+    print "Fizz buzz counting up to {}".format(input)
+    for number in range(1,input):
+        if (number % 3 ==0) and (number % 5 == 0):
+            print "fizzbuzz"
+
+        elif number % 3 == 0:
+            print "fizz"
+
+        elif number % 5 == 0:
+            print "buzz"
+
+        else:
+            print number
+
+fizzbuzz(input)

14703603d51e947b1cc35e346ae0c06483b9961d
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-06-25 07:20:23 -0400

    updated requirements

---
+++ projects/flask-hello-world/hello_world.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def say_hi():
+    return "Hello world!"
+
+
+@app.route("/hello/<name>")
+def hi_person(name):
+    return "Hello {}!".format(name.title())
+
+if __name__ == '__main__':
+    app.run()
--- projects/snippets-app/snippets.py
+++ projects/snippets-app/snippets.py
@@ -1,17 +1,22 @@
 import logging
 import sys
 import argparse
+import psycopg2
 
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect("dbname='snippets' user='action' host='localhost'")
+logging.debug("Database connection established.")
 
 
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-
-    Returns the name of the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})", format(name, snippet))
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
 
 
